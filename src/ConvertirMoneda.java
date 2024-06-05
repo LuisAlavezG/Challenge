dd5cc7677f75b009badb86a97c7df44f431d6685
LuisAlavezG <luisarturogarciaalavez@gmail.com>
2024-06-05 12:46:40 -0600

    Proyecto terminado

--- src/ConvertirMoneda.java
+++ src/ConvertirMoneda.java
@@ -1,2 +1,18 @@
-public class ConvertirMonedaes{
+import java.util.Scanner;
+
+public class ConvertirMoneda {
+
+    public static void convertir(String monedaBase, String monedaTarget, ConsultarMoneda consulta, Scanner lectura) {
+        double cantidad;
+        double cantidadConvertida;
+
+        Monedas monedas = consulta.buscarMoneda(monedaBase, monedaTarget);
+        System.out.println("La Tasa tasa de convesion es " + monedaBase + "=" + monedas.conversion_rate());
+        System.out.println("Ingresa la cantidad de " + monedaBase);
+        cantidad = Double.parseDouble(lectura.nextLine());
+        cantidadConvertida = cantidad * monedas.conversion_rate();
+        System.out.println(cantidad+" "+monedaBase + " = " + cantidadConvertida + " " + monedas.target_code());
+    }
+
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        ConsultarMoneda consulta = new ConsultarMoneda();
 
+        int opcion = 0;
+
+        while(opcion != 7 ) {
+
+              System.out.println( " ************************************** \n" +
+                      " Conversor de monedas      \n\n " +
+                      "Ingresa la opcion a elegir \n\n " +
+                      "1. Dollar a Peso Argentino.  \n " +
+                      "2. Peso Argentino a Dollar   \n " +
+                      "3. Dollar a Real Brasileño   \n " +
+                      "4. Real Brasileño a Dollar   \n " +
+                      "5. Dollar a Peso Colombiano  \n " +
+                      "6. Peso Colombiano a Dollar  \n " +
+                      "7. Salir                     \n "
+                      );
+              opcion = lectura.nextInt();
+              lectura.nextLine();
+
+              switch (opcion) {
+
+                  case 1:
+                      ConvertirMoneda.convertir("USD", "ARS", consulta, lectura );
+                     break;
+
+                  case 2:
+                      ConvertirMoneda.convertir("ARS", "USD", consulta, lectura);
+                      break;
+                  case 3:
+                      ConvertirMoneda.convertir("USD", "BRL", consulta, lectura);
+                      break;
+                  case 4:
+                      ConvertirMoneda.convertir("BRL", "USD", consulta, lectura);
+                     break;
+                  case 5:
+                      ConvertirMoneda.convertir("USD", "COP", consulta, lectura);
+                      break;
+                  case 6:
+                      ConvertirMoneda.convertir("COP", "USD", consulta, lectura);
+                      break;
+                  case 7:
+                      System.out.println("Saliendo ...... ");
+                      break;
+
+                  default:
+                      System.out.println("Opcion no  valida ");
+                      break;
+
+              }
+        }
     }
 }
